f31f9a0071a9b383242d86f4e4d372c22bf98d13
Y <pingyun0425@gmail.com>
2026-04-14 20:14:08 +0800

    feat: balls reflection on collision (known issue: balls may escape wall bounds)

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -5,6 +5,7 @@ public class BallScript : MonoBehaviour
     public float speed = 5f;
     public ShooterScript shooter;
     public Vector2 dir;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,5 +17,13 @@ public class BallScript : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3)dir * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided");
+        if (collision.gameObject.layer != 3) {
+            dir = Vector2.Reflect(dir, collision.contacts[0].normal);
+        }
     }
 }
