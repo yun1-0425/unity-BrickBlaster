d064bfc08aa6f50c12e6fdfd3c2eed0057dd6e51
Y <pingyun0425@gmail.com>
2026-04-15 17:02:59 +0800

    fix: ball escaping wall bounds
    - move the calculation of ball movement from Update() to FixedUpdate()
    - replace transform.position with rb.MovePosition()
    - add clamp walls

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -3,20 +3,43 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     public float speed = 5f;
+    public Rigidbody2D rb;
+
     public ShooterScript shooter;
     public Vector2 dir;
+    public float leftWall = -4;
+    public float rightWall = 4;
+    public float topWall = 6;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         shooter = GameObject.FindGameObjectWithTag("Shooter").GetComponent<ShooterScript>();
+
         dir = shooter.direction;
+        leftWall = shooter.leftWall;
+        rightWall = shooter.rightWall;
+        topWall = shooter.topWall;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate() // 球的移動運算 Update 改 FixedUpdate 才不會有漏算球跑出去問題
     {
-        transform.position += (Vector3)dir * speed * Time.deltaTime;
+        //transform.position += (Vector3)dir * speed * Time.deltaTime; //transform 寫法是瞬間移動，rb 是有路徑的移動
+        rb.MovePosition(rb.position + dir * speed * Time.deltaTime);
+
+        // clamp, just in case
+        if (transform.position.x < leftWall)
+        {
+            transform.position = new Vector3(leftWall, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > rightWall)
+        {
+            transform.position = new Vector3(rightWall, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y > topWall)
+        {
+            transform.position = new Vector3(transform.position.x, topWall, transform.position.z);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
